4e8678c036748eda6f017cfdd8a31c62f9e9a95a
Minh Tan <ngminhtan0410@gmail.com>
2025-01-23 17:16:51 +0700

    17h16 23.01.2025

--- K22416C/SalesManagement/UI/LoginMainWindowExt.py
+++ K22416C/SalesManagement/UI/LoginMainWindowExt.py
@@ -45,4 +45,3 @@ class LoginMainWindowExt(Ui_MainWindow):
 
 
 
-
--- K22416C/SalesManagement/UI/MainProgramMainWindowExt.py
+++ K22416C/SalesManagement/UI/MainProgramMainWindowExt.py
@@ -1,3 +1,5 @@
+import sys
+
 from SalesManagement.UI.MainProgramMainWindow import Ui_MainWindow
 
 class MainProgramMainWindowExt(Ui_MainWindow):
@@ -6,3 +8,12 @@ class MainProgramMainWindowExt(Ui_MainWindow):
         self.MainWindow = MainWindow
     def showWindow(self):
         self.MainWindow.show()
+
+    def showWindow(self):
+        self.MainWindow.show()
+
+    def setupSignalAndSlot(self):
+        self.actionthoatphanmem.triggered.connect(self.xuly_thoat)
+
+    def xuly_thoat(self):
+        sys.exit(0)
