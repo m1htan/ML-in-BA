ef671e606ba2402afde3a57748bf12d93e9f8de4
Minh Tan <ngminhtan0410@gmail.com>
2025-05-19 12:32:19 +0700

    ...

--- LearnQTableWidgetPart3/MyApp.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from MainWindowEx import MainWindowEx
-
-app=QApplication([])
-myWindow= MainWindowEx()
-myWindow.setupUi(QMainWindow())
-myWindow.show()
-app.exec()
